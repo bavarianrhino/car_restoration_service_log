920ca3e72dd8fbd3158abbabde0524be4d57d387
github username <github email address>
2019-01-22 17:12:43 -0500

    random save???

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @owner = Owner.find_by(username: params[:username])
+    if @owner && @owner.authenticate(params[:password])
+      session[:owner_id] = @owner.id
+      redirect_to vehicles_path
+    else
+      @error = "Login failed"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:person_id] = nil
+    redirect_to login_path
+  end
+end

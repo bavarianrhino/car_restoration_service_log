a336325f2a97a3f1909ae092c8feaea1485073d2
github username <github email address>
2019-01-22 15:55:34 -0500

    added session/login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :services
   resources :vehicles
   resources :owners
+
+  get '/login', to: 'sessions#new', as: 'login' 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

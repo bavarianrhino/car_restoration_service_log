729db1dfba4878eb5be17551da1290ac4f27255c
github username <github email address>
2019-01-22 15:51:05 -0500

    new password/username migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_042407) do
+ActiveRecord::Schema.define(version: 2019_01_22_204254) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_01_21_042407) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
   create_table "parts", force: :cascade do |t|
